f7da32b4f42a5f2d51eb40e9271bde4199f6bd03
DeGenius360 <degenius8@gmail.com>
2023-03-13 14:47:52 +0000

    Initial commit

---
+++ web-app/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
+
+async_mode = None
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+@app.route('/')
+def index():
+    return render_template('index.html', sync_mode = socketio.async_mode)
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+            {'data': message['data'], 'count':
+session['receive_count']})
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
